6ebdd07cf8dbc925fae374d2a25c0432cb3a5769
unknown <nathaniel.putera@yahoo.com>
2019-03-08 13:52:14 +0700

    first commit

---
+++ src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <div id="app">
+    <router-link to="/">
+      <img src="./assets/logo.png">
+    </router-link>
+    <div style="margin-top:10px">
+      <router-link to="/contact-us"  class="button">
+        Contact Us
+      </router-link>
+    </div>
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+.button{
+  background-color: lightblue;
+  padding: 5px;
+  border-radius:5px;
+  text-decoration: none;
+  color: black;
+  font-weight: bold;
+}
+</style>
---
+++ src/pages/ContactUsPage.vue
@@ -0,0 +1,51 @@
+<template>
+    <div>
+        <div>
+            <label>Full Name:
+                <input 
+                type="text" 
+                class="contact-input"
+                v-model="formData.fullName"></label>
+        </div>
+        <div>
+            <label>Email:
+                <input
+                type="email" 
+                v-model="formData.email">
+            </label>
+        </div>
+        <label>Description: 
+            <textarea v-model="formData.description"></textarea></label>
+        <div><button>Submit</button></div>
+        <div>
+            Data:
+            <div>
+                {{formData}}
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'ContactUsPage',
+    data(){
+        return{
+            formData:{
+                fullName:'',
+                email:'',
+                description:''
+            }
+        }
+    }
+}
+</script>
+
+
+<style <style scoped>
+    label{
+        display: block;
+        padding-top: 20px;
+    }
+</style>
+
